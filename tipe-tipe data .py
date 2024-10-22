4a0f304c675706acf450b7b0cdcd9226069ff391
Hasanah977 <hasanabakri911@gmail.com>
2024-10-23 00:43:29 +0800

    Add files via upload

---
+++ tipe-tipe data .py
@@ -0,0 +1,35 @@
+# Tipe data String
+a = "nama saya zekc"
+print (a)
+
+#Tipe data integer
+b = 25
+print (b)
+
+#Tipe data float
+c = 51,6
+print (c)
+
+#Tipe data Complex Number
+d = 5j
+print (d)
+
+#Tipe data boolean
+e = True
+print (e)
+
+#Tipe data list
+f = [ "satu, dua, tiga"]
+print (f)
+
+#Tipe data tuple
+g = (" tiga, dua, satu")
+print(g)
+
+#Tipe data set
+h = {"satu, dua, tiga, empat"}
+print(h)
+
+#Tipe data dictonary
+i = {"satu":1, "dua":2.13, "tiga":"a", "empat": True}
+print(i)
